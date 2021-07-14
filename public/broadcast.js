b96a75bf80fab30158d5ad763771fe143f5c36de
Timezonedesign <60486296+Timezone-design@users.noreply.github.com>
2021-07-14 08:54:16 -0700

    Update broadcast.js

--- public/broadcast.js
+++ public/broadcast.js
@@ -7,7 +7,7 @@ const config = {
   ]
 };
 const videoElement = document.querySelector("video");
-const socket = io.connect("https://test.yklmrealestate.com:443");
+const socket = io.connect("https://test.yklmrealestate.com");
  
 
 document.getElementById("start").onclick = function () {
