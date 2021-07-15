44802bff1e994517404b63bc80e6c5a3c3d51259
Timezonedesign <60486296+Timezone-design@users.noreply.github.com>
2021-07-14 18:35:31 -0700

    Update broadcast.js

--- public/broadcast.js
+++ public/broadcast.js
@@ -7,7 +7,7 @@ const config = {
   ]
 };
 const videoElement = document.querySelector("video");
-const socket = io.connect("54.188.5.126:3000");
+const socket = io.connect("54.188.5.126:60000");
  
 
 document.getElementById("start").onclick = function () {
